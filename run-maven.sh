f13f6dfba84e7867e2cdc3f33df811f7eb5c8bb4
milo-minderbinder <minderbinder.enterprises@gmail.com>
2015-02-11 16:32:53 -0500

    Switched to Oracle Java 8 SDK base image

--- run-maven.sh
+++ run-maven.sh
@@ -6,4 +6,4 @@ else
 	maven_projects_dir="${1}"
 fi
 echo "Running maven image with ${maven_projects_dir} mounted on /mnt/maven-projects"
-docker run -it --rm -v ${maven_projects_dir}:/mnt/maven-projects --name maven mminderbinder/maven:oracle-java7 bash
+docker run -it --rm -v ${maven_projects_dir}:/mnt/maven-projects --name maven mminderbinder/maven:oracle-java8 bash

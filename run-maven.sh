4221c65c110fdb1f55ee2b5fab3bafa401862bce
Milo Minderbinder <minderbinder.enterprises@gmail.com>
2015-02-13 14:03:52 -0500

    run-maven.sh now permits a '-t' switch to specify a tag name

--- run-maven.sh
+++ run-maven.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
-	maven_projects_dir=$(pwd)
-else
-	maven_projects_dir="${1}"
+
+usage() {
+	echo "Usage: ${0} [-t <tag>] [<path/to/volume>]"
+	echo "Maven Docker run script"
+	exit 0
+}
+
+maven_projects_dir="$(pwd)"
+tag="latest"
+
+if [[ -n "$1" ]]; then
+	if [[ $1 == "-h" ]]; then
+		usage
+	fi
+	if [[ $1 == "-t" ]]; then
+		tag="${2}"
+		if [[ -n "$3" ]]; then
+			maven_projects_dir="${3}"
+		fi
+	else
+		maven_projects_dir="${1}"
+	fi
 fi
-echo "Running maven image with ${maven_projects_dir} mounted on /mnt/maven-projects"
-docker run -it --rm -v ${maven_projects_dir}:/mnt/maven-projects --name maven mminderbinder/maven:oracle-java8 bash
+
+echo "Running maven:${tag} image with ${maven_projects_dir} mounted on /mnt/maven-projects"
+docker run -it --rm -v ${maven_projects_dir}:/mnt/maven-projects --name maven mminderbinder/maven:${tag} bash
